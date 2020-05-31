6b2c0850ce8d3dba77c7f278e4ff5d4f504b9601
Josh Feinsilber <joshfeinsilber@gmail.com>
2020-05-31 09:38:29 -0700

    Updates

---
+++ precache-manifest.cd018a34980eaf3e6bc8102d681b0901.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e214bb4a6d5261c8097a23c88e237d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebed37b9d2f4f845c1bb",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "64e08039a4835859b5b2",
+    "url": "/static/js/2.8b13572d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8b13572d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebed37b9d2f4f845c1bb",
+    "url": "/static/js/main.1642b1ba.chunk.js"
+  },
+  {
+    "revision": "34ba351e0c170673545e",
+    "url": "/static/js/runtime-main.501d1807.js"
+  }
+]);
